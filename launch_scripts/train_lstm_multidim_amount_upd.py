c81384a5671f3b4b1d86a71af13fcbead4c659ed
Gaechka777 <boeva.gl@phystech.edu>
2022-03-27 22:39:23 +0300

    model with information about material

--- launch_scripts/train_lstm_multidim_amount_upd.py
+++ launch_scripts/train_lstm_multidim_amount_upd.py
@@ -1,10 +1,6 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-
-import sys
-sys.path.append('C:\\Users\\boeva\\Beer\\Repository', )
-
 from models.regression.lstm_multidim_amount_upd import RegressionNet
 from data_preparation.data_reader_upd import OrderReader
 from data_preparation.dataset_preparation_upd import OrderDataset
@@ -12,7 +8,7 @@ from sklearn.metrics import r2_score, mean_absolute_percentage_error
 import numpy as np
 from sacred import Experiment
 import os
-#from utils.earlystopping import EarlyStopping
+from utils.earlystopping import EarlyStopping
 import json
 from tqdm import tqdm
 from utils.utils import own_r2_metric
@@ -33,10 +29,10 @@ from utils.utils import own_r2_metric
 # @ex.automain
 def train():
 
-    data_folder = "C:\\Users\\boeva\\Beer\\"
-    train_file = "df_beer_train.csv"
+    data_folder = "../initial_data/"
+    train_file = "df_beer_train_nn.csv"
     test_file = "df_beer_test.csv"
-    valid_file = "df_beer_test.csv"
+    valid_file = "df_beer_valid_nn.csv"
     look_back = 3
 
     num_epochs = 500
@@ -84,7 +80,7 @@ def train():
     valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=dataloader_num_workers)
 
     os.makedirs(results_folder+'checkpoints/', exist_ok=True)
-    #early_stopping = EarlyStopping(patience=early_stopping_patience, verbose=True, path=checkpoint)
+    early_stopping = EarlyStopping(patience=early_stopping_patience, verbose=True, path=checkpoint)
 
     for epoch in range(1, num_epochs+1):
         net.train(True)
@@ -98,20 +94,16 @@ def train():
              batch_current_cat, batch_mask_current_cat, batch_onehot_current_cat,
              batch_num_arr, batch_id_arr, batch_target] = batch_arrays
             optimizer.zero_grad()
-            output, batch_onehot_current_cat, mask_current_cat = net(batch_cat_arr, batch_mask_cat,
+            output = net(batch_cat_arr, batch_mask_cat,
                          batch_current_cat, batch_mask_current_cat, batch_onehot_current_cat,
-                         batch_num_arr, batch_id_arr)
-            output = output.reshape(-1)
+                         batch_num_arr, batch_id_arr).reshape(-1)
             nonzero_indices = batch_onehot_current_cat.reshape(-1).nonzero()
             batch_predicted_amounts = output[nonzero_indices]
-
-            batch_target = batch_target * mask_current_cat.reshape(-1, mask_current_cat.shape[1]) - (mask_current_cat.reshape(-1, mask_current_cat.shape[1]) - 1) * train_dataset.amount_padding_value
             batch_gt_amounts = batch_target.reshape(-1)[(batch_target.reshape(-1) - train_dataset.amount_padding_value).nonzero()]
 
             train_predicted_amounts.extend(batch_predicted_amounts.detach().cpu().tolist())
             train_gt_amounts.extend(batch_gt_amounts.detach().cpu().tolist())
-            if batch_predicted_amounts.shape[0] != batch_gt_amounts.shape[0]:
-                continue
+
             loss = regr_loss(batch_predicted_amounts, batch_gt_amounts)
             epoch_train_loss += loss.item()
             # loss = -own_r2_metric(batch_predicted_amounts, batch_gt_amounts)
@@ -122,7 +114,7 @@ def train():
         # epoch_train_r2 = r2_score(np.array(train_gt_amounts, dtype=np.float64).reshape(-1, 1),
         #                           np.array(train_predicted_amounts, dtype=np.float64).reshape(-1, 1))
         epoch_train_r2 = r2_score(train_dataset.mms.inverse_transform(np.array(train_gt_amounts, dtype=np.float64).reshape(-1, 1)),
-                                  train_dataset.mms.inverse_transform(np.array(train_predicted_amounts[:len(train_gt_amounts)], dtype=np.float64).reshape(-1, 1)))
+                                  train_dataset.mms.inverse_transform(np.array(train_predicted_amounts, dtype=np.float64).reshape(-1, 1)))
         print(f'Epoch {epoch}/{num_epochs} || Train loss {epoch_train_loss} || Train r2_score {epoch_train_r2}')
 
         print('Validation...')
@@ -136,22 +128,16 @@ def train():
                 [batch_cat_arr, batch_mask_cat,
                  batch_current_cat, batch_mask_current_cat, batch_onehot_current_cat,
                  batch_num_arr, batch_id_arr, batch_target] = batch_arrays
-                output, batch_onehot_current_cat, mask_current_cat = net(batch_cat_arr, batch_mask_cat,
-                                                                         batch_current_cat, batch_mask_current_cat,
-                                                                         batch_onehot_current_cat,
-                                                                         batch_num_arr, batch_id_arr)
-                output = output.reshape(-1)
+                output = net(batch_cat_arr, batch_mask_cat,
+                             batch_current_cat, batch_mask_current_cat, batch_onehot_current_cat,
+                             batch_num_arr, batch_id_arr).reshape(-1)
                 nonzero_indices = batch_onehot_current_cat.reshape(-1).nonzero()
                 batch_predicted_amounts = output[nonzero_indices]
-
-                batch_target = batch_target * mask_current_cat.reshape(-1, mask_current_cat.shape[1]) - (mask_current_cat.reshape(-1, mask_current_cat.shape[1]) - 1) * train_dataset.amount_padding_value
                 batch_gt_amounts = batch_target.reshape(-1)[(batch_target.reshape(-1) - train_dataset.amount_padding_value).nonzero()]
 
                 valid_predicted_amounts.extend(batch_predicted_amounts.detach().cpu().tolist())
                 valid_gt_amounts.extend(batch_gt_amounts.detach().cpu().tolist())
 
-                if batch_predicted_amounts.shape[0] != batch_gt_amounts.shape[0]:
-                    continue
                 loss = regr_loss(batch_predicted_amounts, batch_gt_amounts)
                 epoch_valid_loss += loss.item()
 
@@ -159,14 +145,14 @@ def train():
         # r2_metric_valid = r2_score(np.array(valid_gt_amounts, dtype=np.float64),
         #                            np.array(valid_predicted_amounts, dtype=np.float64))
         epoch_valid_r2 = r2_score(valid_dataset.mms.inverse_transform(np.array(valid_gt_amounts, dtype=np.float64).reshape(-1, 1)),
-                                  valid_dataset.mms.inverse_transform(np.array(valid_predicted_amounts[:len(valid_gt_amounts)], dtype=np.float64).reshape(-1, 1)))
+                                  valid_dataset.mms.inverse_transform(np.array(valid_predicted_amounts, dtype=np.float64).reshape(-1, 1)))
         print(f'Epoch {epoch}/{num_epochs} || Valid loss {epoch_valid_loss} || Valid r2_score {epoch_valid_r2}')
         scheduler.step(-epoch_valid_r2)
 
-        #early_stopping(-epoch_valid_r2, net)
-        #if early_stopping.early_stop:
-        #    print('Early stopping')
-        #    break
+        early_stopping(-epoch_valid_r2, net)
+        if early_stopping.early_stop:
+            print('Early stopping')
+            break
 
 #----------------------------------------------
     net = RegressionNet(linear_num_feat_dim, cat_embedding_dim, lstm_hidden_dim,
@@ -174,7 +160,7 @@ def train():
                         id_embedding_dim, linear_concat1_dim, linear_concat2_dim).to(device)
     net.load_state_dict(torch.load(checkpoint, map_location=device))
     net.train(False)
-    print('Testing already done as Valid')
+    print('Testing...')
     test_predicted_amounts = []
     test_gt_amounts = []
     with torch.no_grad():
