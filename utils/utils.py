144c9d92f124e8366e7aca4c5297bb2144e12ea4
E-Kovtun <lizakovtun1003@mail.ru>
2022-03-29 19:20:36 +0300

    utils_update

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, KBinsDiscretizer
 import torch
 
 
@@ -17,10 +18,8 @@ def get_fitted_scaler(df_train, df_test, df_valid, feature_name):
     return mms
 
 
-def get_max_cat_len(df_final, cat_feature):
-    history_lengths = []
-    for i in range(len(df_final)):
-        history_lengths.append(len(df_final.loc[i, cat_feature]))
+def get_max_cat_len(df_train, df_test, df_valid, cat_feature):
+    history_lengths = pd.concat([df_train[cat_feature], df_valid[cat_feature], df_test[cat_feature]], axis=0).apply(lambda x: len(x))
     return np.max(history_lengths)
 
 
@@ -30,3 +29,16 @@ def own_r2_metric(output, target):
     ss_tot = torch.sum((target - target_mean) ** 2)
     r2 = 1 - ss_res / ss_tot
     return r2
+
+
+def get_fitted_discretizer(df_train, df_test, df_valid, amount_feature):
+    all_amounts = pd.concat([df_train[amount_feature], df_test[amount_feature], df_valid[amount_feature]]).values
+    amount_discretizer = KBinsDiscretizer(n_bins=9, encode='ordinal', strategy='quantile')
+    amount_discretizer.fit(all_amounts.reshape(-1, 1))
+    return amount_discretizer
+
+
+def get_max_dt(df_train, df_test, df_valid, dt_fetaure):
+    all_dt = pd.concat([df_train[dt_fetaure], df_test[dt_fetaure], df_valid[dt_fetaure]]).values
+    return np.max(all_dt).astype(np.int64)
+
