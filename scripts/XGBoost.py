8730e5c2f94d0f4843928480be554be478e5d56f
Gaechka777 <boeva.gl@phystech.edu>
2022-06-09 13:59:19 +0300

    xgboost update

--- scripts/XGBoost.py
+++ scripts/XGBoost.py
@@ -4,12 +4,14 @@ sys.path.append("../",)
 
 #from launch_scripts.train_lstm_material import mean_patk, mean_ratk, mapk
 from data_preparation.data_reader import OrderReader
+from torch.utils.data import DataLoader
 import numpy as np
 import torch.nn.functional as F
 import os
 import json
 from tqdm import tqdm
 import xgboost as xgb
+from utils.roc_auc_metrics import calculate_roc_auc_metrics
 from sklearn.multioutput import MultiOutputClassifier
 import pickle as pkl
 
@@ -40,9 +42,15 @@ def train():
     look_back = 3
 
     prepared_folder = "datasets/data/prepared/order/"
-
+    batch_size = 32
+    dataloader_num_workers = 2
     rand_seed_list = [1, 2, 3, 4, 5]
-
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+    all_roc_auc_micro = []
+    all_roc_auc_macro = []
     for rand_seed in tqdm(rand_seed_list):
         torch.manual_seed(rand_seed)
 
@@ -69,13 +77,26 @@ def train():
         #os.makedirs(results_folder+'checkpoints/', exist_ok=True)
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                                       num_workers=dataloader_num_workers)
-        
-        one_hot_id = F.one_hot(train_dataset.id_arr)
+        entire_batch_cat_arr = []
+        entire_batch_current_cat = []
+        entire_batch_dt_arr = []
+        entire_batch_amount_arr = []
+        entire_batch_id_arr = []
+        for batch_ind, batch_arrays in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
+            batch_arrays = [arr.to(device) for arr in batch_arrays]
+            [batch_cat_arr, batch_current_cat, batch_dt_arr, batch_amount_arr, batch_id_arr] = batch_arrays
+            entire_batch_cat_arr.append(batch_cat_arr)
+            entire_batch_current_cat.append(batch_current_cat)
+            entire_batch_dt_arr.append(batch_dt_arr)
+            entire_batch_amount_arr.append(batch_amount_arr)
+            entire_batch_id_arr.append(batch_id_arr)
+
+        one_hot_id = F.one_hot(entire_batch_id_arr, train_dataset.id_vocab_size)
         print(one_hot_id)
-        one_hot_label = F.one_hot(train_dataset.mask_cat * train_dataset.cat_arr)
-        X_train = torch.cat((train_dataset.id_arr,
-                             torch.cat((train_dataset.mask_cat * train_dataset.cat_arr, train_dataset.num_arr),
-                                       axis=2).reshape(len(train_dataset.id_arr), -1)), axis=1)
+        one_hot_label = F.one_hot(entire_batch_current_cat, train_dataset.cat_vocab_size)
+        X_train = torch.cat((one_hot_id,
+                             torch.cat((one_hot_label, entire_batch_amount_arr),
+                                       axis=2).reshape(len(one_hot_id), -1)), axis=1)
 
         # create XGBoost instance with default hyper-parameters
         xgb_estimator = xgb.XGBClassifier(objective='binary:logistic')
@@ -99,16 +120,30 @@ def train():
                                        train_test_dataset.num_arr[-test_len:]),
                                       axis=2).reshape(len(train_test_dataset.id_arr[-test_len:]), -1)), axis=1)
 
-        all_output = torch.Tensor(multilabel_model.predict(X_test))
-        all_gt = train_test_dataset.onehot_current_cat[-test_len:]
-        test_mean_patk = {i: mean_patk(all_output, all_gt, k=i) for i in range(1, 5)}
-        test_mean_ratk = {i: mean_ratk(all_output, all_gt, k=i) for i in range(1, 5)}
-        test_mapk = {i: mapk(all_output, all_gt, k=i) for i in range(1, 5)}
+        # all_output = torch.Tensor(multilabel_model.predict(X_test))
+        # all_gt = train_test_dataset.onehot_current_cat[-test_len:]
+        # test_mean_patk = {i: mean_patk(all_output, all_gt, k=i) for i in range(1, 5)}
+        # test_mean_ratk = {i: mean_ratk(all_output, all_gt, k=i) for i in range(1, 5)}
+        # test_mapk = {i: mapk(all_output, all_gt, k=i) for i in range(1, 5)}
+        #
+        # print(f'Test Precision@k {test_mean_patk} || Test Recall@k {test_mean_ratk}|| Test MAP@k {test_mapk})')
+        #
+        # with open(results_folder + f'{os.path.splitext(os.path.basename(checkpoint))[0]}.json', 'w', encoding='utf-8') as f:
+        #     json.dump({'test_precision@k': test_mean_patk, 'test_recall@k': test_mean_ratk, 'test_map@k': test_mapk}, f)
+        metrics_dict = calculate_roc_auc_metrics(np.array(all_scores), np.array(all_gt))
+        all_roc_auc_micro.append(metrics_dict['roc_auc_micro'])
+        all_roc_auc_macro.append(metrics_dict['roc_auc_macro'])
+        os.makedirs(os.path.join('results/', dataset_name, model_name), exist_ok=True)
+        with open(os.path.join('results/', dataset_name, model_name,
+                               f'metrics_look_back_{look_back}_seed_{rand_seed}.json'), 'w', encoding='utf-8') as f:
+            json.dump(metrics_dict, f)
 
-        print(f'Test Precision@k {test_mean_patk} || Test Recall@k {test_mean_ratk}|| Test MAP@k {test_mapk})')
+    df = pd.DataFrame(data=[[np.mean(all_roc_auc_micro), np.std(all_roc_auc_micro)],
+                            [np.mean(all_roc_auc_macro), np.std(all_roc_auc_macro)]],
+                      index=['roc_auc_micro', 'roc_auc_macro'],
+                      columns=['mean', 'std'])
+    df.to_csv(os.path.join('results/', dataset_name, model_name, f'metrics_final.csv'))
 
-        with open(results_folder + f'{os.path.splitext(os.path.basename(checkpoint))[0]}.json', 'w', encoding='utf-8') as f:
-            json.dump({'test_precision@k': test_mean_patk, 'test_recall@k': test_mean_ratk, 'test_map@k': test_mapk}, f)
 
 if __name__ == '__main__':
     train()
